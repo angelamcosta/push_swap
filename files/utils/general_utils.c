b255a39f6aa1962d43826d25ca96973bd5e84f9d
Angela Lima <angelamcostalima@icloud.com>
2023-03-17 16:57:32 +0000

    :fire: remove unused functions

--- files/utils/general_utils.c
+++ files/utils/general_utils.c
@@ -6,42 +6,13 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:46:17 by anlima            #+#    #+#             */
-/*   Updated: 2023/03/17 16:22:34 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/17 16:56:07 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int		ft_strlen(char *str);
-char	*ft_strjoin(char *str1, char *str2);
 int		ft_atoi(char *str);
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str && str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char *str1, char *str2)
-{
-	char	*new_string;
-	int		i;
-
-	new_string = (char *)malloc(ft_strlen(str1) + ft_strlen(str2) + 1);
-	if (!new_string)
-		return (0);
-	i = 0;
-	while (str1 && *str1)
-		new_string[i++] = *str1++;
-	while (str2 && *str2)
-		new_string[i++] = *str2++;
-	new_string[i] = 0;
-	return (new_string);
-}
 
 int	ft_atoi(char *str)
 {
