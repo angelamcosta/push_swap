cb135c842218369b52128af8ca5a284635913963
Angela Lima <angelamcostalima@icloud.com>
2023-03-17 16:23:48 +0000

    :construction_worker: update makefile and header
    update makefile and header to reflect changes in code structure

--- files/push_swap.h
+++ files/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 13:14:31 by anlima            #+#    #+#             */
-/*   Updated: 2023/02/10 16:14:14 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/17 16:22:27 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,26 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_stack
+{
+	int	*a;
+	int	*b;
+	int	size_a;
+	int	size_b;
+	int	cont_a;
+	int	cont_b;
+}				t_stack;
+
+void	ft_push_a(void);
+void	ft_push_b(void);
+void	ft_swap_a(void);
+void	ft_swap_b(void);
+void	ft_swap_ss(void);
 int		ft_strlen(char *str);
 char	*ft_strjoin(char *str1, char *str2);
 int		ft_atoi(char *str);
+t_stack	*stacks(void);
+int		create_stacks(void);
+void	populate_stacks(char **argv);
 
 #endif
