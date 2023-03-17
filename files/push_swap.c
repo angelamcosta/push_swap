972fbf5f61ab299445ef4bf41c88a15b54c5a13f
Angela Lima <angelamcostalima@icloud.com>
2023-03-17 16:24:30 +0000

    :art: refactor push_swap main

--- files/push_swap.c
+++ files/push_swap.c
@@ -6,32 +6,19 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 13:09:02 by anlima            #+#    #+#             */
-/*   Updated: 2023/02/10 15:05:15 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/17 16:21:55 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
 int	main(int argc, char **argv)
 {	
-	int		*a;
-	int		i;
-	int		j;
-
-	a = (int *)malloc((sizeof(int)) * (argc - 1));
-	if (!a)
-		return (0);
 	if (argc > 1)
 	{
-		i = 0;
-		j = i;
-		while (argv && argv[++i])
-			a[j++] = ft_atoi(argv[i]);
-		// testando a `stack`
-		j = 0;
-		while (a && a[j])
-			printf("%i\n", a[j++]);
+		stacks()->size_a = argc - 1;
+		stacks()->size_b = stacks()->size_a - 3;
+		populate_stacks(argv);
 	}
 	else
 		write(1, "You need to pass arguments to populate stack a!\n", 48);
