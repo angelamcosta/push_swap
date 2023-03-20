5fe1c7147a1d86eac821b09f20453e5014717860
Angela Lima <angelamcostalima@icloud.com>
2023-03-20 17:15:43 +0000

    :ambulance: fixes sorting

--- files/utils/push_swap_utils.c
+++ files/utils/push_swap_utils.c
@@ -6,23 +6,17 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 13:15:45 by anlima            #+#    #+#             */
-/*   Updated: 2023/03/17 18:03:19 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/20 17:11:04 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-t_stack	*stacks(void);
 int		create_stacks(void);
 void	populate_stacks(char **argv);
 void	sort_stack_a(void);
-
-t_stack	*stacks(void)
-{
-	static t_stack	stacks;
-
-	return (&stacks);
-}
+void	sort_stack_all(void);
+void	sort_all(int i, int j);
 
 int	create_stacks(void)
 {
@@ -46,63 +40,82 @@ void	populate_stacks(char **argv)
 	if (create_stacks())
 	{
 		i = 0;
-		j = stacks()->size_b;
-		if (j == 0)
-			i++;
-		else
+		j = 0;
+		while (argv && argv[++i])
 		{
-			while (argv && argv[j] && j >= 0)
-				stacks()->b[i++] = ft_atoi(argv[j--]);
-			j = 0;
+			stacks()->a[j++] = ft_atoi(argv[i]);
+			stacks()->cont_a++;
 		}
-		while (argv && argv[i])
-			stacks()->a[j++] = ft_atoi(argv[i++]);
+		while (stacks()->cont_a != 3)
+		{
+			if (stacks()->a[0] > stacks()->a[0])
+				ft_swap_a();
+			ft_push_b();
+		}
+		sort_stack_a();
 	}
-	stacks()->cont_a = stacks()->size_a - stacks()->size_b;
-	stacks()->cont_b = stacks()->size_b;
 }
 
 void	sort_stack_a(void)
 {
-	int	*temp;
+	int	*a;
 
-	temp = stacks()->a;
-	if (temp[0] > temp[1] && temp[0] < temp[2])
-		ft_swap_a();
-	else if (temp[0] > temp[1] && temp[0] > temp[2])
+	a = stacks()->a;
+	if (a[0] > a[1] && a[0] > a[2])
 		ft_rotate_a();
-	else if (temp[1] > temp[0] && temp[1] > temp[2])
-	{
+	else if (a[1] > a[0] && a[1] > a[2])
 		ft_reverser_a();
+	if (a[0] > a[1])
 		ft_swap_a();
-	}
 }
 
-void	sort_all(void)
+void	sort_stack_all(void)
 {
-	int	*a;
-	int	*b;
 	int	i;
 	int	j;
+	int	*a;
 
-	sort_stack_a();
-	a = stacks()->a;
-	b = stacks()->b;
 	i = 0;
-	while (stacks()->cont_b > 0)
+	j = 1;
+	a = stacks()->a;
+	while (a[i])
 	{
-		while (b[0] > a[i])
-			i++;
-		if (i == 0)
-			ft_push_a();
-		else
+		if (a[0] < a[i + 1] || !(a[i + 1]))
+			break ;
+		i++;
+	}
+	if (i == 0)
+		return ;
+	if (i == 1)
+		ft_swap_a();
+	else
+		sort_all(i, j);
+}
+
+void	sort_all(int i, int j)
+{	
+	if (i <= ((stacks()->cont_a - 1) / 2))
+	{
+		ft_swap_a();
+		while (j++ < i)
 		{
-			j = i;
-			while (j-- > 0)
-				ft_rotate_a();
-			ft_push_a();
-			while (i-- > 0)
-				ft_reverser_a();
+			ft_rotate_a();
+			ft_swap_a();
 		}
+		j = 1;
+		while (j++ < i)
+			ft_reverser_a();
+	}
+	else
+	{
+		j = stacks()->cont_a - 1 - i;
+		while (j-- > 0)
+		{
+			ft_reverser_a();
+			ft_swap_a();
+		}
+		j = stacks()->cont_a - i;
+		while (j-- > 0)
+			ft_rotate_a();
 	}
 }
