ef0698d49635e971113a7476c7e57a16067698e9
Angela Lima <angelamcostalima@icloud.com>
2023-03-20 14:02:50 +0000

    :construction_worker: update header

--- files/push_swap.h
+++ files/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 13:14:31 by anlima            #+#    #+#             */
-/*   Updated: 2023/03/17 18:14:19 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/20 14:02:20 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,25 @@ typedef struct s_stack
 
 void	ft_push_a(void);
 void	ft_push_b(void);
+void	ft_push(int *a, int *b, int *cont_a, int *cont_b);
 void	ft_reverser_a(void);
 void	ft_reverser_b(void);
 void	ft_reverser_rr(void);
+void	ft_reverser(int *a, int cont_a);
 void	ft_rotate_a(void);
 void	ft_rotate_b(void);
 void	ft_rotate_rr(void);
+void	ft_rotate(int *a, int cont_a);
 void	ft_swap_a(void);
 void	ft_swap_b(void);
 void	ft_swap_ss(void);
-int		ft_atoi(char *str);
+void	ft_swap(int *a);
 t_stack	*stacks(void);
+int		ft_atoi(char *str);
 int		create_stacks(void);
 void	populate_stacks(char **argv);
 void	sort_stack_a(void);
+void	sort_stack_b(void);
 void	sort_all(void);
 
 #endif
