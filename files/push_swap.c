238b6df3379de5c55f1205e49f2a927b7efb141b
Angela Lima <angelamcostalima@icloud.com>
2023-05-24 17:01:05 +0100

    :recycle: refactor push_swap.c code

--- files/push_swap.c
+++ files/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 13:09:02 by anlima            #+#    #+#             */
-/*   Updated: 2023/05/20 23:09:18 by anlima           ###   ########.fr       */
+/*   Updated: 2023/05/24 15:55:33 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ int	main(int argc, char **argv)
 {	
 	if (argc <= 1)
 		return (0);
-	if (is_overflow(&argv[1]) || !(verify_str(&argv[1])))
+	if (!(verify_str(&argv[1])))
 		return (write(1, "Error\n", 6));
 	populate_stacks(&argv[1]);
 	if (has_repeats(stacks()->a))
+	{
+		free_stacks();
 		return (write(1, "Error\n", 6));
+	}
 	while (ft_lstsize(stacks()->b) > 0)
 	{
 		ft_push_a();
-		sort_stack_all();
+		ft_sort();
 	}
+	free_stacks();
 	return (0);
 }
